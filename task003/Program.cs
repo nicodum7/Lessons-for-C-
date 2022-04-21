ded46b0cd511eeda35dc75cdf05df4e6995f9884
igoryan <nkulinigor1984@icloud.com>
2022-04-21 21:22:09 +0200

    Задача003

---
+++ task003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя - ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юлькама")
+{
+Console.WriteLine(" - иди спать уже!");
+}
+else
+{
+    Console.Write("О, привет!");
+}
